7606a7d89639fdd326f68a9febb8406f406bb58d
Emil Müller <emil@alumis.no>
2019-04-10 14:38:13 +0200

    no message

---
+++ Alumis.Collections.RedBlackTree/RedBlackTreeNodeColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alumis.Collections
+{
+    public enum RedBlackTreeNodeColor
+    {
+        Red,
+        Black
+    }
+}
